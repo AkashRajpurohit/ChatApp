0536b22cc0b01d24d0271506c5e9e64972760423
Akashrajpurohit <akashrajpurohit.009@gmail.com>
2018-10-13 19:30:08 +0530

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Static folder
+app.use(express.static(path.join(__dirname, "../public")));
+
+const port = process.env.PORT || 4200;
+app.listen(port, () => console.log(`Server up and running on port ${port}...`));
