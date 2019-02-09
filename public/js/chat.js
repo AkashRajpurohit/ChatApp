d7ca387294978ed670734dd7b50c28bebb3137e7
Akash Rajpurohit <akash009.dev@gmail.com>
2019-02-09 12:18:43 +0530

    Chaned from people to room name

--- public/js/chat.js
+++ public/js/chat.js
@@ -24,12 +24,17 @@ function scrollToBottom() {
 
 socket.on("connect", function() {
   let params = $.deparam(window.location.search);
-  params.room = params.room.toLowerCase();
+  const room = params.room;
+  if(params.room) {
+    params.room = room.toLowerCase();
+  }
 
   socket.emit("join", params, function(err) {
     if (err) {
       alert(err);
       window.location.href = "/";
+    } else {
+      document.querySelector("#chat__roomname").innerHTML = room;
     }
   });
 });
